2db7c1cdc15f60a8d1955bae98501faa33537137
“majestyz3” <majid.zarkesh@hashicorp.com>
2023-04-26 12:43:48 -0400

    update placeholder var description

--- variables.tf
+++ variables.tf
@@ -46,5 +46,5 @@ variable "width" {
 
 variable "placeholder" {
   default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+  description = "Image-as-a-service URL. Some other fun ones to try are placekitten.com,fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
